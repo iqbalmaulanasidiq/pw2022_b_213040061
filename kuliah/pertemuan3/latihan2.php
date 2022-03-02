cbb0f68086e69bb0b2752b582166d5caae37cad8
IqbalMaulanaSidiq <iqbal.213040061@mail.unpas.ac.id>
2022-03-02 14:52:25 +0700

    tugas pertemuan 3 latihan 2

---
+++ kuliah/pertemuan3/latihan2.php
@@ -0,0 +1,14 @@
+<?php 
+    // Pengkondisian / Percabangan
+    // if else, if else if else, ternary, switch
+
+    $x =20;
+    if($x <20){
+        echo "benar";
+    }elseif($x == 20){
+        echo "bingo!";
+    }else{
+        echo "salah";
+    
+    }
+?>
