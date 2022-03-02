9b80d4e57ab8afce0fc04018edd8e8e0b10ab1de
IqbalMaulanaSidiq <iqbal.213040061@mail.unpas.ac.id>
2022-03-02 15:06:10 +0700

    Hasil akhir pertemuan 3 latihan 1

--- kuliah/pertemuan3/latihan1.php
+++ kuliah/pertemuan3/latihan1.php
@@ -5,6 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style> 
+    .warna-baris {
+        background-color: silver;
+    }
+    </style>
 </head>
 <body>
 
@@ -49,10 +54,14 @@ echo "<hr>";
          </table>
             <br>
 
-        <!-- cara lain -->
+    <!-- cara lain -->
     <table border="1" cellpadding="10" cellspacing="0">
-        <?php for($i=1;$i<=3;$i++): ?>
+        <?php for($i=1;$i<=5;$i++): ?>
+            <?php if($i % 2 == 1): ?>
+            <tr class="warna-baris">
+            <?php else: ?>
             <tr>
+            <?php endif; ?>
                 <?php for($j =1;$j<=5;$j++){ ?>
                    <td><?php echo"$i,$j" ?></td> 
                 <?php } ?>  
